da8ad129f262948718c4f34388abe6333ef7e806
Prachi <prachiaky53@gmail.com>
2026-04-12 22:29:28 +0530

    commit

---
+++ practice/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Work from './work.jsx'
+
+const App = () => {
+  return (
+    <>
+    <Work/>
+    <h1>PHYSICS</h1> 
+    </>
+  )
+}
+
+export default App
---
+++ practice/src/Work.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <h2>Hello prachiiiiii</h2>
+    
+  )
+}
+
+export default Work
